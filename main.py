c9781b251b164c9f9787b73c650b7ddb2d27354c
nzukow2 <nzukow2@illinois.edu>
2022-01-06 12:54:38 -0600

    Updated push

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
-#test
-#
+import pandas
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+
+#before import, install pandas and numpy modules at terminal
+# (windows uses "pip install", mac uses "pip3 --version")
+
+
+# CSV obtained from: https://github.com/owid/covid-19-data/tree/master/public/data
+# Respective website from Our World in Data: https://ourworldindata.org/coronavirus-source-data
+
+class Visualization:
+    def __init__(self):
+        pass
+
+    def dataLoad(self):
+        pandas.read_csv("owid-covid-data.csv")
+
+#def countryViewer
+
+
+
+
+
+df = pd.read_csv("owid-covid-data.csv")
+pd.options.display.max_rows = 10000000
+print(df)
